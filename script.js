cfdcf88c3799e3104bebd4ed26e7c7375f98858c
PrahladKumar <prahladchoudhary7980@gmail.com>
2025-12-16 18:06:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+function zero_one_two_sorting(arr) {
+    let low = 0;
+    let mid = 0;
+    let high = arr.length - 1;
+
+    const swap = (i, j) => {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    };
+
+    while (mid <= high) {
+        if (arr[mid] === 0) {
+            swap(low, mid);
+            low++;
+            mid++;
+        } else if (arr[mid] === 1) {
+            mid++;
+        } else {
+            swap(mid, high);
+            high--;
+        }
+    }
+}
